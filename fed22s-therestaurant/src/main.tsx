cfe3638f2784c42976964b644442e2cee9ca26a0
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-05-31 12:49:03 +0200

    (feat): setup react router

--- fed22s-therestaurant/src/main.tsx
+++ fed22s-therestaurant/src/main.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import './scss/main.scss'
+import LandingPage from './views/LandingPage';
+import BookingPage from './views/BookingPage';
+import Layout from './components/Layout';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage></LandingPage>
+      },
+      {
+        path: "/book",
+        element: <BookingPage></BookingPage>
+      },
+    ],
+  },
+]);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
+
+
