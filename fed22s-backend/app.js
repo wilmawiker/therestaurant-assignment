25a0ec43de0488d38e00e9231926ef65abadaa62
Wilma Wiker <wikerwilma@gmail.com>
2023-05-31 10:15:09 +0200

    basics for app.js express app

---
+++ fed22s-backend/app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bookingRoutes = require("./routes/bookingRoutes");
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`Processing ${req.method} request to ${req.path}`);
+  next();
+});
+
+app.use("/api/v1/bookings", bookingRoutes);
+
+const port = process.env.PORT || 5000;
+async function run() {
+  try {
+    // Connect to MongoDB database (via Mongoose)
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+
+    // Start server; listen to requests on port
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+run();
