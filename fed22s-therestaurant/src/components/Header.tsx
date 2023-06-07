5a6cd18c744bdb27b9c761fc72cacd8766953158
Wilma Wiker <wikerwilma@gmail.com>
2023-06-07 13:15:53 +0200

    added link to contact page to header

--- fed22s-therestaurant/src/components/Header.tsx
+++ fed22s-therestaurant/src/components/Header.tsx
@@ -1,17 +1,19 @@
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
 
 export const Header = () => {
-    return (
-        <nav id="nav">
-        <ul>
-            <li>
-                <Link to="/">Hem</Link>
-            </li>
-            <li>
-                <Link to="/book">Boka Bord</Link>
-            </li>
-        </ul>
+  return (
+    <nav id="nav">
+      <ul>
+        <li>
+          <Link to="/">Hem</Link>
+        </li>
+        <li>
+          <Link to="/book">Boka Bord</Link>
+        </li>
+        <li>
+          <Link to="/contact">Kontakt</Link>
+        </li>
+      </ul>
     </nav>
-    )
-}
+  );
+};
