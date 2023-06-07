dbe6867792697f6cd8230b82b8bb7b1553c9cd66
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-06-07 23:45:27 +0200

    (feat): show times after selecting date

--- fed22s-therestaurant/src/components/SittingForm.tsx
+++ fed22s-therestaurant/src/components/SittingForm.tsx
@@ -1,34 +1,44 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import { IBooking, defaultBooking } from "../models/IBooking";
 import setBookingLs from "../utils/setLS";
-import bookingFromLS from "../utils/getLS";
+import { ValuePiece } from "../utils/valuePiece";
 
-const SittingForm = () => {
-    console.log(bookingFromLS);
+interface SittingFormProps {
+    booking: IBooking;
+    add: (booking: IBooking) => void;
+    showTime: boolean;
     
-    const [sitting, setSitting] = useState<IBooking>(bookingFromLS);
+}
+
+const SittingForm = ({booking, add, showTime}: SittingFormProps) => {
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const name = e.target.name;
-        setSitting({...sitting, [name]: e.target.value})    
+        add({...booking, [name]: e.target.value}) 
+        console.log(booking);  
     }
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    setBookingLs(sitting);
-    console.log(sitting);
+    setBookingLs(booking);
+    console.log(booking);
   }
-    return <form onSubmit={handleSubmit}>
-        <h3>Välkommen</h3>
+    return ( 
+    <div>
+        {showTime ? 
+        <form onSubmit={handleSubmit}>
         <div>
-        <label htmlFor="18">Sittning kl 18</label>
+        <label htmlFor="18">kl 18</label>
         <input type="radio" name="sitting" id="18" value={1} onChange={handleChange}/>
         </div>
         <div>
-        <label htmlFor="21">Sittning kl 21</label>
+        <label htmlFor="21">kl 21</label>
         <input type="radio" name="sitting" id="21" value={2} onChange={handleChange}/>
         </div>
         <button type="submit">Välj</button>
-    </form>
+        </form>
+        : null }
+    </div>
+    )
 }
 
 export default SittingForm;
