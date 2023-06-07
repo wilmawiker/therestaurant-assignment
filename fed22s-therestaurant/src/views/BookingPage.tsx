e9b92600e4d269fe0b7e71cbd220801af199cec3
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-06-07 13:28:26 +0200

    (feat): order of bookinginfo

--- fed22s-therestaurant/src/views/BookingPage.tsx
+++ fed22s-therestaurant/src/views/BookingPage.tsx
@@ -4,8 +4,8 @@ import SittingForm from "../components/sittingForm";
 const BookingPage = () => {
     
     return <><h3>Bokningssida</h3>
-    <SittingForm></SittingForm>
     <DateForm></DateForm>
+    <SittingForm></SittingForm>
     </>
 
 }
