9947b0277b50c48142fc6196483cb27113dbabff
Ida <ida.nacka@gmail.com>
2023-06-07 10:34:23 +0200

    simple landing page

---
+++ fed22s-therestaurant/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+
+export const Header = () => {
+    return (
+        <nav id="nav">
+        <ul>
+            <li>
+                <Link to="/">Hem</Link>
+            </li>
+            <li>
+                <Link to="/book">Boka Bord</Link>
+            </li>
+        </ul>
+    </nav>
+    )
+}
--- fed22s-therestaurant/src/views/LandingPage.tsx
+++ fed22s-therestaurant/src/views/LandingPage.tsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+import { Header } from "../components/Header"
+import { Footer } from "../components/Footer"
+
 const LandingPage = () => {
-    return <h1>Startsida</h1>
+    return (
+        <>
+        <Header></Header>
+        <div>
+        <Link to="/book">
+        <button>Boka Bord</button>
+        </Link>
+       </div>
+       <Footer></Footer>  
+        </>
+    )
 }
 
 export default LandingPage;
