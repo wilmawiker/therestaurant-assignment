b413c6985fffb70ddfcd9f0588dc6e92de6ab11e
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-06-07 13:28:08 +0200

    (feat): chosen timeslot to ls

--- fed22s-therestaurant/src/components/SittingForm.tsx
+++ fed22s-therestaurant/src/components/SittingForm.tsx
@@ -1,18 +1,31 @@
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { IBooking, defaultBooking } from "../models/IBooking";
+import setBookingLs from "../utils/setLS";
+import bookingFromLS from "../utils/getLS";
 
 const SittingForm = () => {
+    console.log(bookingFromLS);
+    
+    const [sitting, setSitting] = useState<IBooking>(bookingFromLS);
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const name = e.target.name;
+        setSitting({...sitting, [name]: e.target.value})    
+    }
+
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+    setBookingLs(sitting);
+    console.log(sitting);
   }
     return <form onSubmit={handleSubmit}>
         <h3>Välkommen</h3>
         <div>
         <label htmlFor="18">Sittning kl 18</label>
-        <input type="radio" name="timeSlot" id="18" />
+        <input type="radio" name="sitting" id="18" value={1} onChange={handleChange}/>
         </div>
         <div>
         <label htmlFor="21">Sittning kl 21</label>
-        <input type="radio" name="timeSlot" id="21" />
+        <input type="radio" name="sitting" id="21" value={2} onChange={handleChange}/>
         </div>
         <button type="submit">Välj</button>
     </form>
