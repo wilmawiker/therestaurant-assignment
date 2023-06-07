37d89da20c527cd02704480688bf24392f9c4563
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-06-08 00:02:54 +0200

    (feat): IBooking working for api-call and forms

--- fed22s-therestaurant/src/models/IBooking.ts
+++ fed22s-therestaurant/src/models/IBooking.ts
@@ -1,24 +1,21 @@
 export interface IBooking {
-
-    numberOfPeople: number;
-    sitting: number;
-    email: string;
-    date: Date;
-}
-
-export const defaultBooking: IBooking = {
-    numberOfPeople: 0,
-    sitting: 1,
-    email: "",
-    date: new Date()
-}
-
-  id: number;
-  table: Number;
+  id?: Number;
+  table?: Number;
   numberOfPeople: Number;
   sitting: Number;
   email: String;
   phoneNumber: String;
   date: Date;
 }
+
+export const defaultBooking: IBooking = {
+    numberOfPeople: 0,
+    sitting: 1,
+    email: "",
+    phoneNumber: "",
+    date: new Date()
+}
+
+ 
+
 
