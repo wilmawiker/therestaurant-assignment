bf88a44bad7588321d397c4ffb33ca71124afc5f
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-06-07 13:27:32 +0200

    (feat): add dropdown for number of guests

--- fed22s-therestaurant/src/components/DateForm.tsx
+++ fed22s-therestaurant/src/components/DateForm.tsx
@@ -1,17 +1,37 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
 import { useForm } from "react-hook-form";
+import { IBooking, defaultBooking } from "../models/IBooking";
+import bookingFromLS from "../utils/getLS";
+import setBookingLs from "../utils/setLS";
 
 
 const DateForm = () => {
-    const { handleSubmit, register, formState: { errors } } = useForm();
-    const onSubmit = (values: any) => console.log(values);
-    const [date, changeDate] = useState(new Date());
+    const [booking, setBooking] = useState<IBooking>(defaultBooking);
+    const [date, setDate] = useState(new Date());
+
+    const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        const name = e.target.name;
+        setBooking({...booking, [name]: e.target.value});
+        console.log(booking);  
+    }
+
+    const handleDateChange = (e: ChangeEvent<Calendar>) => {
+
+        setBooking({...booking, date: date})
+    }
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        console.log(booking);
+        
+        setBookingLs(booking)
+    }
     return <>
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit}>
     <label htmlFor="guests">Antal gäster:</label>
-    <select name="guests" id="guests">
+    <select name="numberOfPeople" id="guests" onChange={handleChange}>
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -19,9 +39,9 @@ const DateForm = () => {
         <option value="5">5</option>
         <option value="6">6</option>
     </select>
-    <Calendar onChange={changeDate} value={
-        date
-    }/>
+    <Calendar onChange={setDate} value={date}/>
+    <button>Kolla tillgänglighet
+    </button>
 </form>
 <p>{date.toDateString()}</p>
 </>
