34b46e1d03c2ec74a54081908fcbe1a0d887e33e
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-06-08 10:38:37 +0200

    (feat): show info of booking

--- fed22s-therestaurant/src/views/BookingPage.tsx
+++ fed22s-therestaurant/src/views/BookingPage.tsx
@@ -3,16 +3,29 @@ import CustomerForm from "../components/CustomerForm";
 import DateForm from "../components/DateForm";
 import { IBooking, defaultBooking } from "../models/IBooking";
 
-
 const BookingPage = () => {
-    const [booking, setBooking] = useState<IBooking>(defaultBooking);
-    const [showForm, setShowForm] = useState(false);
-    return <><h3>Bokningssida</h3>
-    <DateForm booking={booking} add={setBooking} show={setShowForm}></DateForm>
-    <CustomerForm booking={booking}></CustomerForm>
-    
+  const [booking, setBooking] = useState<IBooking>(defaultBooking);
+  const [showForm, setShowForm] = useState(false);
+  return (
+    <>
+      <h3>Bokningssida</h3>
+      <DateForm
+        booking={booking}
+        add={setBooking}
+        show={setShowForm}
+      ></DateForm>
+      <CustomerForm booking={booking} add={setBooking}></CustomerForm>
+      <div>
+        <p>{booking.firstName}</p>
+        <p>{booking.lastName}</p>
+        <p>{booking.email}</p>
+        <p>{booking.phoneNumber}</p>
+        <p>{booking.date.toDateString()}</p>
+        <p>{booking.numberOfPeople.toString()}</p>
+        <p>{booking.sitting.toString()}</p>
+      </div>
     </>
-
-}
+  );
+};
 
 export default BookingPage;
