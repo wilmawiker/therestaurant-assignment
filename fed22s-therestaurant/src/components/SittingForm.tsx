82a285ea647d664291c2eaf59d9167a6ac6a7f58
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-06-08 10:38:16 +0200

    (feat): show form toggle

--- fed22s-therestaurant/src/components/SittingForm.tsx
+++ fed22s-therestaurant/src/components/SittingForm.tsx
@@ -4,41 +4,53 @@ import setBookingLs from "../utils/setLS";
 import { ValuePiece } from "../utils/valuePiece";
 
 interface SittingFormProps {
-    booking: IBooking;
-    add: (booking: IBooking) => void;
-    showTime: boolean;
-    
+  booking: IBooking;
+  add: (booking: IBooking) => void;
+  showTime: boolean;
 }
 
-const SittingForm = ({booking, add, showTime}: SittingFormProps) => {
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const name = e.target.name;
-        add({...booking, [name]: e.target.value}) 
-        console.log(booking);  
-    }
+const SittingForm = ({ booking, add, showTime }: SittingFormProps) => {
+  const [showCustomerForm, setShowForm] = useState(false);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    add({ ...booking, [name]: e.target.value });
+    console.log(booking);
+  };
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     setBookingLs(booking);
-    console.log(booking);
-  }
-    return ( 
+    setShowForm(true);
+  };
+  return (
     <div>
-        {showTime ? 
+      {showTime ? (
         <form onSubmit={handleSubmit}>
-        <div>
-        <label htmlFor="18">kl 18</label>
-        <input type="radio" name="sitting" id="18" value={1} onChange={handleChange}/>
-        </div>
-        <div>
-        <label htmlFor="21">kl 21</label>
-        <input type="radio" name="sitting" id="21" value={2} onChange={handleChange}/>
-        </div>
-        <button type="submit">Välj</button>
+          <div>
+            <label htmlFor="18">kl 18</label>
+            <input
+              type="radio"
+              name="sitting"
+              id="18"
+              value={1}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="21">kl 21</label>
+            <input
+              type="radio"
+              name="sitting"
+              id="21"
+              value={2}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Välj</button>
         </form>
-        : null }
+      ) : null}
     </div>
-    )
-}
+  );
+};
 
 export default SittingForm;
