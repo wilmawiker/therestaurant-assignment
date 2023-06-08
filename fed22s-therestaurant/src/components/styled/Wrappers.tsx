368749223833b96797dcd8926299975a513f88e6
Wilma Wiker <wikerwilma@gmail.com>
2023-06-08 14:38:14 +0200

    added genreal wrapper that centers main part of page

--- fed22s-therestaurant/src/components/styled/Wrappers.tsx
+++ fed22s-therestaurant/src/components/styled/Wrappers.tsx
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
+export const GeneralWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
