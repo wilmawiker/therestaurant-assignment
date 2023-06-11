4f6d03c73453dccae03e6a4ecd19dfcc77d7e38d
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-06-11 16:51:30 +0200

    (feat): styled h1 component

--- fed22s-therestaurant/src/components/styled/StyledH1.tsx
+++ fed22s-therestaurant/src/components/styled/StyledH1.tsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
-export const StyledH1 = styled.h1`
+interface StyledH1Props {
+  fontSize: string;
+}
+
+export const StyledH1 = styled.h1<StyledH1Props>`
   text-align: center;
   font-family: "Permanent Marker";
   color: white;
+  font-size: ${(props: StyledH1Props) => props.fontSize || "2rem"};
 `;
