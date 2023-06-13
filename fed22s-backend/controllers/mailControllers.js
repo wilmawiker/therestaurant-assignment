f9d45688eb1764eb4ef46f29a5304bd875f88dbb
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-06-13 22:28:46 +0200

    (feat): add message for mailcontroller

--- fed22s-backend/controllers/mailControllers.js
+++ fed22s-backend/controllers/mailControllers.js
@@ -1,9 +1,23 @@
 const sendEmail = require("../utils/sendEmail");
 
-exports.sendEmail = async (req, res) => {
-  const { email } = req.body;
+exports.createEmail = async (req, res) => {
+  const { email, firstName, date, id } = req.body;
 
   try {
-    await sendEmail();
-  } catch (error) {}
+    const send_to = email;
+    const sent_from = process.env.EMAIL_USER;
+    const subject = "Bokningsbekräftelse WAIO Restaurang";
+    const message = `
+    <h3>Hej ${firstName}!</h3>
+    <p>Tack för din bokning hos oss på <b>WAIO</b> ${date}.</p>
+    <p>För att ändra din bokning tryck här: <button>Ändra bokning</button> <a>${id}</a></p>
+    <h4>Allt gott, WAIO!</h4>
+    `;
+    console.log(send_to);
+
+    await sendEmail(send_to, sent_from, subject, message);
+    return res.status(200).json({ success: true, message: "Email sent" });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
 };
