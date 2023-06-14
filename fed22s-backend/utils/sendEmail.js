a85730553acdedf8bfd080d65735542d59f71b36
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-06-14 11:14:21 +0200

    (feat): secure false

--- fed22s-backend/utils/sendEmail.js
+++ fed22s-backend/utils/sendEmail.js
@@ -4,13 +4,11 @@ const sendEmail = async (send_to, sent_from, subject, message) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: "587",
+    secure: false,
 
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
-    },
-    tls: {
-      rejectUnauthorized: false,
     },
   });
 
