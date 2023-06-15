d0aeb9e16f9ecb6fe34211772033d66ed90d0060
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-06-15 15:58:22 +0200

    (feat): text in mail

--- fed22s-backend/controllers/mailControllers.js
+++ fed22s-backend/controllers/mailControllers.js
@@ -10,9 +10,20 @@ exports.createEmail = async (req, res) => {
     const subject = "Bokningsbekräftelse WAIO Restaurang";
     const message = `
     <h3>Hej ${firstName}!</h3>
-    <p>Tack för din bokning hos oss på <b>WAIO</b> ${date}.</p>
-    <p>För att ändra din bokning tryck <a href="http://localhost:5173/admin/${_id}">här</a></p>
+    <p>Tack för din bokning hos oss på <b>WAIO</b> ${date}. </br>
+    För att ändra din bokning tryck <a href="http://localhost:5173/admin/${_id}">här</a></p>
+    __________________________
+    
     <h4>Allt gott, WAIO!</h4>
+
+    __________________________
+
+    <h5>WAIO</h5>
+              <p>Vägen 12</p>
+              <p>123 45 Sthlm</p>
+              <p>Tel. 123-456 78 90</p>
+              <p>info@awo.se</p>
+
     `;
 
     await sendEmail(send_to, sent_from, subject, message);
