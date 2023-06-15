c84db028087b9e891d9320d89e5f0da1765c8c4a
Wilma Wiker <wikerwilma@gmail.com>
2023-06-15 11:36:40 +0200

    update booking from admin page

--- fed22s-therestaurant/src/views/AdminPage.tsx
+++ fed22s-therestaurant/src/views/AdminPage.tsx
@@ -1,13 +1,20 @@
-import { useEffect, useState } from "react";
-import { IBooking } from "./../models/IBooking";
+import { useEffect, useState, useReducer } from "react";
+import { IBooking, defaultBooking } from "./../models/IBooking";
 import { getAllBookings } from "../services/bookingServices";
 import setBookingsLs from "../utils/setLS";
 import FilterBookings from "../components/FilterBookings";
 import { AdminTable } from "../components/AdminTable";
 import { StyledH1 } from "../components/styled/StyledH1";
 
+import BookingReducer from "../reducers/BookingReducer";
+import {
+  BookingContext,
+  BookingDispatchContext,
+} from "../contexts/BookingContext";
+
 const AdminPage = () => {
   const [bookings, setBookings] = useState<IBooking[]>([]);
+  const [booking, dispatch] = useReducer(BookingReducer, defaultBooking);
 
   useEffect(() => {
     const getData = async () => {
@@ -21,9 +28,16 @@ const AdminPage = () => {
 
   return (
     <>
-      <StyledH1 fontSize="2rem">Admin</StyledH1>
-      <FilterBookings bookings={bookings} set={setBookings}></FilterBookings>
-      <AdminTable bookings={bookings} set={setBookings}></AdminTable>
+      <BookingContext.Provider value={booking}>
+        <BookingDispatchContext.Provider value={dispatch}>
+          <StyledH1 fontSize="2rem">Admin</StyledH1>
+          <FilterBookings
+            bookings={bookings}
+            set={setBookings}
+          ></FilterBookings>
+          <AdminTable bookings={bookings} set={setBookings}></AdminTable>
+        </BookingDispatchContext.Provider>
+      </BookingContext.Provider>
     </>
   );
 };
